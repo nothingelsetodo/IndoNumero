880a1ae9a77e87eaf9008527fe5685a37766120d
Max <Max@pc-studio>
2018-10-06 19:19:19 +0200

    primo step nessun file java

---
+++ src/it/nasosoft/indonumero/IndoNumeroController.java
@@ -0,0 +1,62 @@
+/**
+ * Sample Skeleton for 'IndoNumero.fxml' Controller Class
+ */
+
+package it.nasosoft.indonumero;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+
+public class IndoNumeroController {
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="btnNuova"
+    private Button btnNuova; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtCurrent"
+    private TextField txtCurrent; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtMax"
+    private TextField txtMax; // Value injected by FXMLLoader
+
+    @FXML // fx:id="boxGioco"
+    private HBox boxGioco; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtTentativo"
+    private TextField txtTentativo; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtLog"
+    private TextArea txtLog; // Value injected by FXMLLoader
+
+    @FXML
+    void handleNuova(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleProva(ActionEvent event) {
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert btnNuova != null : "fx:id=\"btnNuova\" was not injected: check your FXML file 'IndoNumero.fxml'.";
+        assert txtCurrent != null : "fx:id=\"txtCurrent\" was not injected: check your FXML file 'IndoNumero.fxml'.";
+        assert txtMax != null : "fx:id=\"txtMax\" was not injected: check your FXML file 'IndoNumero.fxml'.";
+        assert boxGioco != null : "fx:id=\"boxGioco\" was not injected: check your FXML file 'IndoNumero.fxml'.";
+        assert txtTentativo != null : "fx:id=\"txtTentativo\" was not injected: check your FXML file 'IndoNumero.fxml'.";
+        assert txtLog != null : "fx:id=\"txtLog\" was not injected: check your FXML file 'IndoNumero.fxml'.";
+
+    }
+}
